918f635102614965249a0fd37b53d7d9e1cfe573
zhouzheng <15590942395@163.com>
2023-01-06 15:41:39 +0800

    创建Userpo类

--- wuai-user/src/main/java/com/wuaishop/user/persist/po/User.java
+++ wuai-user/src/main/java/com/wuaishop/user/persist/po/User.java
@@ -1,8 +1,19 @@
 package com.wuaishop.user.persist.po;
 
 import lombok.Data;
+import org.apache.naming.factory.SendMailFactory;
 
 @Data
 public class User {
-
+    private String id;
+    private String userName;
+    private String passWord;
+    private int roleId;
+    private String nickName;
+    private String email;
+    private String phone;
+    private String freeze;
+    private String deptId;
+    private String remark;
+    private String insertTime;
 }
