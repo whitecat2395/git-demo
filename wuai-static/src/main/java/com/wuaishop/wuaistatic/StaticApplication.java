26510e9285454346944d30acbf60a19a8d03b0a5
zhouzheng <15590942395@163.com>
2023-01-06 09:21:02 +0800

    构建静态资源服务器

--- wuai-static/src/main/java/com/wuaishop/wuaistatic/StaticApplication.java
+++ wuai-static/src/main/java/com/wuaishop/wuaistatic/StaticApplication.java
@@ -1,15 +1,12 @@
-package com.wuaishop.gateway.controller;
+package com.wuaishop.wuaistatic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.FeignClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-@FeignClient
-public class GatewayApplication {
+public class StaticApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class,args);
+        SpringApplication.run(StaticApplication.class,args);
     }
 }
